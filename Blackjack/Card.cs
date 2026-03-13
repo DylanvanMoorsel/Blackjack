0bc0f03724a64f993af28137e5dc44cf4ab42af7
djgva <djgva@BIG-DILL-ROC>
2026-03-13 11:10:35 +0100

    Test commit

---
+++ Blackjack/Card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack
+{
+    public class Card
+    {
+        public string Rank { get; set; }  // bijv. "A", "K", "10"
+        public bool IsFaceUp { get; set; } // zichtbaar of verborgen
+    }
+}
