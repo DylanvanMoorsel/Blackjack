068f8cb387f2a40e3137c047e10929999191b9eb
djgva <djgva@BIG-DILL-ROC>
2026-04-10 11:42:19 +0200

    Fixed a glitch where the dealer would only show one of the cards and pushed a different component for the blackjack project

---
+++ Blackjack/Blackjackgame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Blackjack
+{
+    internal class BlackjackGame
+    {
+        Dealer dealer = new Dealer();
+
+        public List<string> playerHand = new List<string>();
+        public List<string> dealerHand = new List<string>();
+
+        public void Deal()
+        {
+            playerHand.Clear();
+            dealerHand.Clear();
+
+            dealerHand.Add(dealer.DrawCard());
+            dealerHand.Add(dealer.DrawCard());
+            playerHand.Add(dealer.DrawCard());
+            playerHand.Add(dealer.DrawCard());
+        }
+
+        public void CheckDealerHand()
+        {
+            while (GetTotal(dealerHand) < 17)
+            {
+                dealerHand.Add(dealer.DrawCard());
+            }
+        }
+
+        public int GetTotal(List<string> hand)
+        {
+            int totaal = 0;
+            foreach (string card in hand)
+            {
+                totaal += dealer.GetCardValue(card);
+            }
+            return totaal;
+        }
+    }
+}
